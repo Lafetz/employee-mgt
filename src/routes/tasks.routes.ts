8b7040e30b228536f6d2df06755eafa7c5109c0c
Lafetz <yafetyohaness@gmail.com>
2023-04-20 18:50:23 +0300

    add task routes

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,73 +1,9 @@
-import { Request, Response, NextFunction } from "express";
-import db from "../db/db";
+import express from "express";
+import * as taskController from "../controllers/tasks.controllers";
+const router = express.Router();
+router.get("/", taskController.listTasks);
+router.post("/", taskController.addTask);
+router.delete("/:taskId", taskController.removeTask);
+router.put("/:taskId", taskController.updateTask);
 
-export const listTasks = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const tasks = await db.task.findMany();
-    res.sendStatus(200).json(tasks);
-  } catch (err) {
-    //  res.status(500).json(err.message);
-  }
-};
-export const addTask = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const task = await db.task.create({
-      data: {
-        name: req.body.name,
-        content: req.body.name,
-        duedate: req.body.name,
-        userId: req.body.name,
-      },
-    });
-    res.sendStatus(201);
-  } catch (err) {
-    //res.status(500).json(err.message);
-  }
-};
-export const removeTask = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    await db.task.delete({
-      where: {
-        id: req.body.id,
-      },
-    });
-    res.sendStatus(200);
-  } catch (err) {
-    //  res.status(500).json(err.message);
-  }
-};
-export const updateTask = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    await db.task.update({
-      where: {
-        id: req.body.id,
-      },
-      data: {
-        name: req.body.name,
-        content: req.body.name,
-        duedate: req.body.name,
-        userId: req.body.name,
-        taskDone: req.body.taskDone,
-      },
-    });
-    res.sendStatus(200);
-  } catch (err) {
-    // res.status(500).json(err.message);
-  }
-};
+export default router;
